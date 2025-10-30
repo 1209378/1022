c74db1adf3c75556512fb5093e84aaf9a6743116
廖清惠 <15183116485@163.com>
2025-10-31 01:09:11 +0800

    Update main.c

--- 3_main.c
+++ 3_main.c
@@ -2,46 +2,50 @@
 //15183116485@163.com
 //廖清惠
 
+#include <stdio.h>
 
-#include <stdio.h>  
+int main()
+ {
+    int n;
+    int isPrime = 1; 
+    int i = 2; 
 
-int main() {
-    int a, b;      
-    char operator;  
-    int result;    
+   
+    printf("请输入一个小于50的正整数：");
+    scanf("%d", &n);
 
-  
-    printf("请按格式输入（示例：3 5 +）：");
-
-    scanf("%d %d %c", &a, &b, &operator);
-
-
-    switch (operator) {
-        case '+':
-            result = a + b;
-            printf("结果：%d\n", result);
-            break;
-        case '-':
-            result = a - b;
-            printf("结果：%d\n", result);
-            break;
-        case '*':
-            result = a * b;
-            printf("结果：%d\n", result);
-            break;
-        case '/':
-          
-            if (b != 0) {
-                result = a / b; 
-                printf("结果：%d\n", result);
-            } else {
-                printf("错误：除数不能为0\n");
-            }
-            break;
-        default:
-        
-            printf("错误：仅支持 +、-、*、/ 四种运算符\n");
+    
+    if (n <= 0 || n >= 50) 
+    {
+        printf("输入无效，请输入小于50的正整数\n");
+        return 1;
     }
 
-    return 0;  
+    
+    if (n == 1)
+    {
+        isPrime = 0; 
+    } else 
+    {
+      
+        while (i < n)
+        {
+            if (n % i == 0) 
+            {
+                isPrime = 0; 
+                break;
+            }
+            i++;
+        }
+    }
+
+   
+    if (isPrime)
+    {
+        printf("密钥安全，密码设置成功\n");
+    } else {
+        printf("密钥不安全，请重新输入\n");
+    }
+
+    return 0;
 }
