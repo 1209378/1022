bc55386652ad255af88721b3ccc8249c729bd175
廖清惠 <15183116485@163.com>
2025-10-31 01:11:03 +0800

    Update main.c
    Replace triangle validation logic with Armstrong number check.

--- 4_main.c
+++ 4_main.c
@@ -4,20 +4,29 @@
 
 #include <stdio.h>
 
-int main() {
-    
-    int a, b, c;
-    
-  
-    printf("请输入三个整数（代表三条线段长度，用空格分隔）：");
-    scanf("%d %d %d", &a, &b, &c);
-    
-   
-    if (a + b > c && a + c > b && b + c > a) {
-        printf("可以组成三角形\n");
-    } else {
-        printf("不能组成三角形\n");
+int main()
+ {
+    int num = 100;
+    while (num <= 999)
+     {
+        
+        int a = num % 10;        
+        int b = (num / 10) % 10;
+        int c = num / 100;      
+        
+      
+        if (a * a * a + b * b * b + c * c * c == num) 
+        {
+            printf("%d", num);
+            
+            if (num != 407) 
+            {
+                printf(" ");
+            }
+        }
+        
+        num++;
     }
-    
+    printf("\n");
     return 0;
 }
